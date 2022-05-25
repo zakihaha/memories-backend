a4ebc6dc705c065c23f27f57563bd33bafd4e389
Muh Zaki Choiruddin <zakichoiruddin24@gmail.com>
2022-05-25 22:01:16 +0700

    update post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,6 +1,7 @@
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js'
 
-export const getPosts = async (req, res) => {
+export const getPosts = async(req, res) => {
     try {
         const PostMessages = await PostMessage.find()
 
@@ -22,4 +23,18 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(409).json({message: error.message})
     }
+}
+
+export const updatePost = async(req, res) => {
+    const {id:_id} = req.params
+    const post = req.body
+
+    if (!mongoose.Types.ObjectId.isValid(_id))  {
+        return res.status(404).send("No post with that id")
+    }
+
+                                                                // agar memiliki id, karena di post tidak ada id
+    const updatePost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true })
+
+    res.json(updatePost)
 }
