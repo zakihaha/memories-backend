23eff2a64f17a69937e827356ac899f8b952bf7c
Muh Zaki Choiruddin <zakichoiruddin24@gmail.com>
2022-05-25 22:01:37 +0700

    delete post

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { getPosts, createPost, updatePost } from "../controllers/PostController.js"
+import { getPosts, createPost, updatePost, deletePost } from "../controllers/PostController.js"
 
 const router = express.Router()
 
 router.get('/', getPosts)
 router.post('/', createPost)
 router.patch('/:id', updatePost)
+router.delete('/:id', deletePost)
 
 export default router
