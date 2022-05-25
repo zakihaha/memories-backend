8368a88ed7fa47ae4fdc13117afe7dc80fd8c4c5
Muh Zaki Choiruddin <zakichoiruddin24@gmail.com>
2022-05-25 23:09:01 +0700

    add dotenv

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import express from "express";
 import bodyParser from 'body-parser'
 import mongoose from "mongoose";
 import cors from 'cors'
-
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js'
 
 const app = express()
+dotenv.config()
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
@@ -13,10 +14,9 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = "mongodb+srv://zakich:LyhNV84pBsLK84Tr@cluster0.wgdv9.mongodb.net/memories?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
     .then(() =>
         app.listen(PORT, () =>
             console.log(`Server running on http://localhost:${PORT}`)
