e5b361b163c402bab41c5b834c678390cf796907
Zaki <muhzakich@students.amikom.ac.id>
2022-05-28 13:19:57 +0700

    remove console log

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -5,8 +5,6 @@ export const getPosts = async (req, res) => {
     try {
         const PostMessages = await PostMessage.find()
 
-        console.log(PostMessages);
-
         res.status(200).json(PostMessages)
     } catch (error) {
         res.status(404).json({ message: error.message })
